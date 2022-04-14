e0d3cb03b6f5ae028b969a8018c81a88fba26975
MuammerSo <muammersomali1@gmail.com>
2022-04-14 22:55:51 +0300

    Initial commit

---
+++ src/test/java/com/fun/entry/EntryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fun.entry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
